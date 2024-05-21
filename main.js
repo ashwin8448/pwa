eb39f8af49885aa57a55de234f7606594659ad0a
AshwinNair <ashbe404@gmail.com>
2024-05-22 02:27:48 +0530

    Added offline support

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js");
-    if (navigator.serviceWorker.controller) {
-      console.log("Current controller: ", navigator.serviceWorker.controller);
-    }
-    navigator.serviceWorker.oncontrollerchange = (e) => {
-      console.log("New service worker activated.");
-    };
-  } else {
-    console.log("Service worker not supported.");
-  }
+  // if ("serviceWorker" in navigator) {
+  //   navigator.serviceWorker.register("/sw.js");
+  //   if (navigator.serviceWorker.controller) {
+  //     console.log("Current controller: ", navigator.serviceWorker.controller);
+  //   }
+  //   navigator.serviceWorker.oncontrollerchange = (e) => {
+  //     console.log("New service worker activated.");
+  //   };
+  // } else {
+  //   console.log("Service worker not supported.");
+  // }
 
   const country_container = document.querySelector(".countries-container");
-  fetch("https://restcountries.com/v3.1/all")
+  fetch("https://restcountries.com/v3.1/currency/rupee")
     .then((response) => response.json())
     .then((response) => {
       country_container.innerHTML = response
         .map(
           (countryInfo) => `<div class="country-card">
-    <img class="flag" src=${countryInfo.flags.svg} alt="aaa" />
+    <img class="flag" src=${countryInfo.flags.svg} alt="flag" />
     <p class="country-name">${countryInfo.name.official}</p>
     <p class="capital">Capital: ${
       countryInfo.capital ? countryInfo.capital.join(", ") : "NA"
     }</p>
-    <p class="continent">Continent: ${countryInfo.continents.join(", ")}</p>
+    <p class="continent">${countryInfo.continents.join(", ")}</p>
   </div>`
         )
         .join(" ");
     });
+
+  const logo = document.querySelector(".logo");
+  logo.addEventListener("click", (event) => {
+    console.log("Fetching details of India");
+    fetch("https://restcountries.com/v3.1/name/india")
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  });
 });
--- sw.js
+++ sw.js
@@ -1,22 +1,75 @@
-const version = 1.0;
+const version = "v1.0";
+const cacheName = `pwaAppCache${version}`;
 
-// Anything outside the events will run first
-// for (i = 10; i < 100; i++) {
-//   console.log(i);
-// }
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(version);
+  await cache.addAll(resources);
+};
 
-self.addEventListener("install", (ev) => {
-  console.log("installed");
+self.addEventListener("install", (event) => {
+  console.log("Installing sw, caching is done in this step");
+  // event.waitUntil(
+  //   addResourcesToCache([
+  //     "/",
+  //     "/index.html",
+  //     "/styles.css",
+  //     "/main.js",
+  //     "/assets/error408.jpg",
+  //   ])
+  // );
 });
 
-self.addEventListener("activate", (ev) => {
-  console.log("activate");
+const deleteCache = async (key) => {
+  await caches.delete(key);
+};
+
+const deleteOldCaches = async () => {
+  const cacheKeepList = [version];
+  const keyList = await caches.keys();
+  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+  await Promise.all(cachesToDelete.map(deleteCache));
+};
+
+self.addEventListener("activate", (event) => {
+  console.log("Activating sw, cleaning is done in this step");
+  // event.waitUntil(deleteOldCaches());
 });
 
-self.addEventListener("fetch", (ev) => {
-  console.log("fetch");
-});
+const putInCache = async (request, response) => {
+  const cache = await caches.open(version);
+  await cache.put(request, response);
+};
 
-self.addEventListener("message", (ev) => {
-  console.log("message");
+const cacheFirst = async ({ request, fallbackUrl }) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
+self.addEventListener("fetch", (event) => {
+  console.log("Intercepting fetch", event.request);
+  // event.respondWith(
+  //   cacheFirst({
+  //     request: event.request,
+  //     fallbackUrl: "/assets/error408.jpg",
+  //   })
+  // );
 });
